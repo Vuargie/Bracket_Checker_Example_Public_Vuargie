3c43eef5e5818b10c5979f9d046d489135651d08
Sander ten Hoor <6397394+Sir-Fenrir@users.noreply.github.com>
2025-11-26 18:36:30 +0100

    Initial commit

--- Bracket_Checker_Example_Public/Program.cs
+++ Bracket_Checker_Example_Public/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Bracket_Checker_Example_Public;
+
+Console.WriteLine(BracketChecker.Check("(hello())"));
